5685474703824e70bdf9d0e6859f316f8ab9f8e7
rahulgupta92 <rahulguptaggn765@gmail.com>
2014-12-17 18:05:15 +0530

    Tutorial-2: Requests and Responses

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, url
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
